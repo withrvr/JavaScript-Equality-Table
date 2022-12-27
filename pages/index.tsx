9259cac082d1ae3765ceea72c525c4cd75610bf3
Raghav Rathi <ytwithrvr@gmail.com>
2022-12-28 00:27:37 +0530

    🏠🔨: updated Home / Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,24 +14,46 @@ const Home = () => {
 				}
 			`}</style> */}
 
-			{/* <h1 className="text-3xl"> Site Currently Under Development </h1> */}
+			<h1 className="text-5xl font-bold mb-6 underline"> JavaScript Equality Table </h1>
 
 			<ol className="text-xl list-disc list-inside my-3 ">
-				<li> Moral of the story / why make this project is : </li>
-				<li>
-					{" "}
-					Always use 3 equals (===) unless you have a good reason to
-					use 2 (==).{" "}
-				</li>
-				<li></li>
+				<li> ( === ) Vs ( == ) </li>
+				<li>  How if( ... ) Statement work in Javascript </li>
+
+				<br />
+
+				<li> Moral of the story / <b>Why ?</b> this project </li>
+				<li> Always use 3 equals ( === ), Unless you have a good reason to use 2 ( == ). </li>
+
+				<br />
+
 				<li> See RESULT ... by clicking on the tabs </li>
 				<li> Hover on tables cell for more detail </li>
+
+				<br />
+
+				<li><a
+					href="https://github.com/withrvr/JavaScript-Equality-Table"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="link"
+				>
+					Github Repository Link, for this Project
+				</a></li>
+				<li><a
+					href="https://github.com/dorey/JavaScript-Equality-Table"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="link"
+				>
+					Credit / Inspired from
+				</a></li>
 			</ol>
 
 			<div className="text-center">
 				<span> Made With ❤ by </span>
 				<a
-					href="http://github.com/withrvr/"
+					href="https://withrvr.bio.link/"
 					target="_blank"
 					rel="noopener noreferrer"
 					className="link"
